fb68a2f2bc12c46e997ba5ed5e23a58524e49fe0
Christian Convey <christian.convey@gmail.com>
2016-08-30 09:20:22 -0400

    New file

--- my-home-dir/bashrc.sh
+++ my-home-dir/bashrc.sh
@@ -3,7 +3,9 @@ declare THIS_SCRIPT_DIR="$(readlink -f $( cd "$( dirname "${BASH_SOURCE[0]}" )" 
 export PATH+=${THIS_SCRIPT_DIR}/bin:${PATH}
 
 HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S: '
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): } '
 
 alias ll="ls -l"
 alias lla="ls -la"
---
+++ setup-new-workstation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is meant to be run on the new workstation.
+
+# TODO: Add symlinks related to config files / scripts stored in this repo.
+
+# TODO: add packages and do apt-file udpate
+
+# TODO: enable inbound ssh
+
+# TODO: establish SSH credentials with spaceman
+
+# TODO: copy spaceman's ssh config file
+
+# TODO: copy ~cconvey/.config/chromium --> new laptop
+
+# TODO: add icons and/or scripts for launching chromium with the particular user I want
+
+# TODO: setup repo clones
+
+# TODO: copy / sync scripts (via scratch repo?)
